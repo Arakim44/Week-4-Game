788c43bbb7a2d9cee2ac4372df24d2ad90282a4b
Ara Kim <ahrakim44@gmail.com>
2016-10-28 23:48:40 -0500

    imges and index working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,7 +22,7 @@ function setGame(){
       //how do I also set the above values?
       //and also, can I add img into this?
       var players = [];
-      [character("Chica",100,8,10,'<img  src="assets/images/Chica2.jpg" alt="Chica" />'),character("Bonnie",120,8,20,'<img src="assets/images/bonnieFix.jpg" alt="Bonnie" />'),character("Foxy",130,10,25,'<img src="assets/images/foxy.png" alt="Foxy" />'),character("Freddy",180,12,30,'<img src="assets/images/nightmare.png" alt="Freddy" />')].forEach(function(itm){
+      [character("Chica",100,8,10,"assets/images/Chica2.jpg"),character("Bonnie",120,8,20,"assets/images/bonnieFix.jpg"),character("Foxy",130,10,25,"assets/images/foxy.png"),character("Freddy",180,12,30,"assets/images/nightmare.png")].forEach(function(itm){
         players.push(itm);
       });
       console.log(players);
@@ -38,11 +38,12 @@ function setGame(){
         players: players, //array above in line 24 i think. but just names..
 
         //method that happens when clicked.
-        whenClicked: function(e){
+      whenClicked: function(e){
           //This extracts the index number of Character number.
           //which is position of the array.
 
           var index = e.target.dataset.index;
+          console.log(e);
           console.log("element index",index);
           var playerClicked = GameThing.players[index];
           console.log("this.players[index]",playerClicked);
@@ -52,14 +53,16 @@ function setGame(){
        //not done writing i don't think. It might come in later.
        //Not sure the purpose yet.
        //Must been clicked first right?
-       selectPlayer: function(index){
+      selectPlayer: function(index){
           GameThing.playerSelected = Gamething.players[index];
           //hopfully playerSelected has all the properties like hp and stuff.
+          p.addEventListener("click",GameThing.whenClicked);
+
         },
 
       //This select your enemy shich is chosen second.
 
-       selectEnemy: function(index){
+      selectEnemy: function(index){
            GameThing.enemySelected = GameThing.player[index];
            console.log(GameThing.enemySelected);
          },
@@ -74,8 +77,8 @@ function setGame(){
           p.id = 'character'+(index+1); //this is id of div for character#
           p.className = "characters";
           p.setAttribute("data-index",index);
-          p.innerHTML =  '<strong data-index="'+index+ '"" >' + player.name+'</strong>';
-          p.innerHTML += player.img;
+          p.innerHTML =  '<strong data-index="'+index+ '" >' + player.name+'</strong>';
+          p.innerHTML += '<img data-index="'+index+'" src="'+player.img+'" alt="'+player.name+'"/>';
 
           rootElement.appendChild(p);//all of the p are adding to the div.
           //well.. the reason why he used addEventListener is that.. maybe click method only works on ids or class?
@@ -97,7 +100,14 @@ $('#startGame').click(function(){
   var gameThing = setGame();
   gameThing.setHTML();
   $('#startGame').hide();
+  // $('#')
 })
+
+
+
+
+
+
 
 
 ///Lucas's example.
